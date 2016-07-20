d9c2207cdc975e4b6b15666819ab95d940956ef2
Tim Zirbel <timzirbel@Tims-MacBook-Pro.local>
2016-07-19 20:51:07 -0500

    trying to remove node_modules

--- src/Components/courses/CoursesPage.js
+++ src/Components/courses/CoursesPage.js
@@ -7,26 +7,10 @@ class CoursePage extends React.Component {
     constructor(props, context)    {
         super(props, context);
 
-        this.onTitleChange = this.onTitleChange.bind(this);
-        this.onClickSave = this.onClickSave.bind(this);
-        
         this.state = {
             course: {title: ""}
         };
 
-    }
-
-    onTitleChange(event)
-    {
-        const course=this.state.course;
-        course.title = event.target.value;
-        this.setState({course: course});
-    }
- 
-    onClickSave(event)
-    {        
-        //alert(`${this.state.course.title}`);
-        this.props.actions.createCourse(this.state.course);
     }
 
     courseRow(course, index){
@@ -38,16 +22,6 @@ class CoursePage extends React.Component {
             <div>
                 <h1>Courses</h1>
                 {this.props.courses.map(this.courseRow)}
-                <h2>Add Course</h2>
-                <input
-                    type="text"
-                    onChange={this.onTitleChange}
-                    value={this.state.course.title}/>
-
-                <input
-                    type="submit"
-                    value="Save"
-                    onClick={this.onClickSave} />
             </div>
         );
     }
